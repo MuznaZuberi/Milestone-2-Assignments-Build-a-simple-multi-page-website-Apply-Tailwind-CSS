34db92410b437586a88e632653f0057d09d01f08
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-10-20 21:02:18 +0500

    Update page.tsx

--- Sizzels_&_Serve Website Nextjs Tailwind CSS/src/app/about_us/page.tsx
+++ Sizzels_&_Serve Website Nextjs Tailwind CSS/src/app/about_us/page.tsx
@@ -21,7 +21,7 @@ export default function About() {
               <h1 className = "font-bold font-serif text-orange-600 text-3xl p-4">The food's quality matches the wonderful taste of the root beer.</h1>
               <h1 className = "font-bold font-serif text-black text-xl pt-4  p-3">Establishing an environment where individuals can relish life's simple joys.
               </h1>
-              <p className = "font-serif text-md p-3 text-justify">Our dynamic dining space guarantees an unforgettable gastronomic experience. Focusing on a range of sizzling dishes, we are committed to providing expertly crafted, delicious meals. The menu features an exciting blend of global cuisines, emphasizing grilled and sizzling platters that will  tantalize your palate. The lively ambiance, combined with outstanding service, makes it an ideal location for family get-togethers, celebrations, or a memorable dining outing.
+              <p className = "font-serif text-md p-3 text-justify">Our lively dining environment promises a remarkable culinary adventure. Focusing on a range of sizzling dishes, we are committed to providing expertly crafted, delicious meals. The menu features an exciting blend of global cuisines, emphasizing grilled and sizzling platters that will  tantalize your palate. The lively ambiance, combined with outstanding service, makes it an ideal location for family get-togethers, celebrations, or a memorable dining outing.
               </p>
               <br/>
               <Link href = "/about_us" className=" text-white bg-orange-600 border-0 py-2 px-6 focus:outline-none hover:bg-black  rounded-xl ml-2">
