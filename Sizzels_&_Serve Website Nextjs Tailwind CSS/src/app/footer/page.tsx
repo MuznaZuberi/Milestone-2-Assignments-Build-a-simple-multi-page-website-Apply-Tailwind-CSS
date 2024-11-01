c8c58cdbd644e82f61b6ec19cba3413021c357cf
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-11-01 17:21:25 +0500

    Update page.tsx

--- Sizzels_&_Serve Website Nextjs Tailwind CSS/src/app/footer/page.tsx
+++ Sizzels_&_Serve Website Nextjs Tailwind CSS/src/app/footer/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 
 
@@ -7,8 +8,8 @@ export default function Footer() {
        <div>
 
         {/* Footer Section Start*/}
-          <footer className="body-font mt-[200px]">
-          <div className="container px-5 mx-auto flex items-center sm:flex-row flex-col">
+          <footer className="body-font">
+          <div className="container px-4 pt-40 pb-2 flex mx-auto items-center sm:flex-row flex-col">
           <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
            <Image 
            className = "w-[90px]"
@@ -16,7 +17,7 @@ export default function Footer() {
            src = {require("../../../img/logo.jpg")}
            />
 
-           <h1 className="text-4xl font-bold font-serif text-black">Sizzels_Serve</h1>
+           <h1 className="text-2xl font-bold font-serif text-black">Sizzels_Serve</h1>
            </a>
            <p className="text-sm text-black font-serif sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
                                                © 2024 Sizzels_Serve —
@@ -26,60 +27,13 @@ export default function Footer() {
             rel="noopener noreferrer"
             target="_blank"
             >
-                                                @By Muzna Amir Zubairi
+                 @By Muzna Amir Zubairi
              </a>
            </p>
-
-
+		  
            <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-               <a className="text-orange-600">
-               <svg
-                 fill="currentColor"
-                 strokeLinecap="round"
-                 strokeLinejoin="round"
-                 strokeWidth={2}
-                 className="w-5 h-5"
-                 viewBox="0 0 24 24"
-                >
-                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
-               </svg>
-               </a>
+            <Link href = "https://www.linkedin.com/in/muzna-amir-z-280135264/" target="blank"  className="ml-3 text-white px-2 py-2 bg-orange-600 hover:bg-black hover:text-white rounded-full">
 
-
-             <a className="ml-3 text-orange-600">
-               <svg
-                 fill="currentColor"
-                 strokeLinecap="round"
-                 strokeLinejoin="round"
-                 strokeWidth={2}
-                 className="w-5 h-5"
-                 viewBox="0 0 24 24"
-                >
-                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
-               </svg>
-             </a>
-
-
-
-            <a className="ml-3 text-orange-600">
-                <svg
-                   fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                 >
-                <rect width={20} height={20} x={2} y={2} rx={5} ry={5} />
-                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
-                </svg>
-            </a>
-
-
-
-
-            <a className="ml-3 text-orange-600">
                 <svg
                    fill="currentColor"
                    stroke="currentColor"
@@ -95,7 +49,7 @@ export default function Footer() {
                  />
                 <circle cx={4} cy={4} r={2} stroke="none" />
                 </svg>
-            </a>
+            </Link>
             </span>
          </div>
       </footer>
